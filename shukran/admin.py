5c0e03849353c73c7c8a73ecdd91c81958b937ba
Endominus <endominus@gmail.com>
2019-06-16 14:21:35 +0400

    Restarting...

--- shukran/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from shukran.models import Thanks
-
-admin.site.register(Thanks)
--- shukran/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShukranConfig(AppConfig):
-    name = 'shukran'
